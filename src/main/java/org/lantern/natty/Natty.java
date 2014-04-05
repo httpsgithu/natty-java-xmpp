0d3451fb266059a9a2a6587c30fa4b5974ec34f2
Myles Horton <myles@getlantern.org>
2014-04-05 11:20:47 -0700

    More fleshed out messaging

--- src/main/java/org/lantern/natty/Natty.java
+++ src/main/java/org/lantern/natty/Natty.java
@@ -5,13 +5,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.RosterListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
 
 public class Natty {
 
@@ -26,34 +35,62 @@ public class Natty {
         // Create a connection to the jabber.org server on a specific port.
         final ConnectionConfiguration config = 
                 new ConnectionConfiguration("talk.google.com", 5222, "gmail.com");
-        /*
-        config.setExpiredCertificatesCheckEnabled(true);
-        config.setNotMatchingDomainCheckEnabled(true);
-        
-        // This will automatically send out a presence message when we log in.
-        config.setSendPresence(true);
-
-        config.setCompressionEnabled(true);
-
-        //config.setRosterLoadedAtLogin(true);
-        config.setReconnectionAllowed(false);
-
-        config.setVerifyChainEnabled(true);
-
-        // This is commented out because the Google Talk signing cert is not
-        // trusted by java by default.
-        config.setVerifyRootCAEnabled(true);
-        config.setSelfSignedCertificateEnabled(false);
-        */
         
         final Connection gtalk = new XMPPConnection(config);
         gtalk.connect();
+        gtalk.addPacketListener(new PacketListener() {
+            
+            @Override
+            public void processPacket(final Packet pack) {
+                System.out.println("PACKET FROM: "+pack.getFrom());
+                final Message msg = (Message) pack;
+                System.out.println(msg.getBody());
+            }
+        }, new PacketFilter() {
+            
+            @Override
+            public boolean accept(final Packet pack) {
+                return pack instanceof Message;
+            }
+        });
         
         System.out.println("CONNNECTED?!?");
         
         final String user = args[0];
         final String pass = args[1];
-        gtalk.login(user, pass);
+        gtalk.login(user, pass, "natty");
+        
+        final Roster roster = gtalk.getRoster();
+        roster.addRosterListener(new RosterListener() {
+            
+            @Override
+            public void presenceChanged(final Presence pres) {
+                final String from = pres.getFrom();
+                System.out.println("GOT PRESENCE: "+from);
+                if (from.contains("natty")) {
+                    final Message msg = new Message();
+                    msg.setTo(from);
+                    msg.setBody("OFFERS AND ANSWERS GO HERE");
+                    gtalk.sendPacket(msg);
+                }
+            }
+            
+            @Override
+            public void entriesUpdated(Collection<String> arg0) {
+            }
+            
+            @Override
+            public void entriesDeleted(Collection<String> arg0) {
+            }
+            
+            @Override
+            public void entriesAdded(Collection<String> arg0) {
+            }
+        });
+        final Collection<RosterEntry> entries = roster.getEntries();
+        for (final RosterEntry re : entries) {
+            System.out.println(re.getUser());
+        }
         
         final String target = args[2];
         runNatty(target);
