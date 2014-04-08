c9823ad6b3244427b24cbc2503d6be81ccc572c7
Todd Blose <todd@lana.io>
2014-04-08 03:42:26 -0700

    pass offer/answer and separate processbuilder into separate class

--- src/main/java/org/lantern/natty/Natty.java
+++ src/main/java/org/lantern/natty/Natty.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -24,97 +27,145 @@ import org.jivesoftware.smack.packet.Presence;
 
 public class Natty {
 
+    private final ConnectionConfiguration config;
+    private final Connection gtalk;
+
+    private String message;
+    private ProcessBuilder builder;
+    private Process process;
+
+    public Natty() {
+      config = new ConnectionConfiguration("talk.google.com", 5222, "gmail.com");
+      gtalk  = new XMPPConnection(config);
+    }
+
     /**
      * @param args user name, password, and target
      * @throws XMPPException 
      * @throws InterruptedException 
      * @throws IOException 
      */
-    public static void main(final String[] args) throws XMPPException, 
+  
+    public void connectGTalk(String user, String pass) throws XMPPException, 
+
         InterruptedException, IOException {
-        // Create a connection to the jabber.org server on a specific port.
-        final ConnectionConfiguration config = 
-                new ConnectionConfiguration("talk.google.com", 5222, "gmail.com");
-        
-        final Connection gtalk = new XMPPConnection(config);
-        gtalk.connect();
-        gtalk.addPacketListener(new PacketListener() {
-            
-            @Override
-            public void processPacket(final Packet pack) {
-                System.out.println("PACKET FROM: "+pack.getFrom());
-                final Message msg = (Message) pack;
-                System.out.println(msg.getBody());
-            }
-        }, new PacketFilter() {
-            
-            @Override
-            public boolean accept(final Packet pack) {
-                return pack instanceof Message;
-            }
-        });
-        
-        System.out.println("CONNNECTED?!?");
-        
-        final String user = args[0];
-        final String pass = args[1];
-        gtalk.login(user, pass, "natty");
-        
-        final Roster roster = gtalk.getRoster();
-        roster.addRosterListener(new RosterListener() {
-            
-            @Override
-            public void presenceChanged(final Presence pres) {
-                final String from = pres.getFrom();
-                System.out.println("GOT PRESENCE: "+from);
-                if (from.contains("natty")) {
-                    final Message msg = new Message();
-                    msg.setTo(from);
-                    msg.setBody("OFFERS AND ANSWERS GO HERE");
-                    gtalk.sendPacket(msg);
-                }
-            }
-            
-            @Override
-            public void entriesUpdated(Collection<String> arg0) {
-            }
-            
-            @Override
-            public void entriesDeleted(Collection<String> arg0) {
-            }
-            
-            @Override
-            public void entriesAdded(Collection<String> arg0) {
-            }
-        });
-        final Collection<RosterEntry> entries = roster.getEntries();
-        for (final RosterEntry re : entries) {
-            System.out.println(re.getUser());
+
+      gtalk.connect();
+      gtalk.addPacketListener(new PacketListener() {
+
+        @Override
+        public void processPacket(final Packet pack) {
+          System.out.println("PACKET FROM: "+pack.getFrom());
+          final Message msg = (Message) pack;
+          System.out.println(msg.getBody());
         }
-        
-        final String target = args[2];
-        runNatty(target);
-        Thread.sleep(20000);
+      }, new PacketFilter() {
+
+        @Override
+        public boolean accept(final Packet pack) {
+          return pack instanceof Message;
+        }
+      });
+
+      System.out.println("CONNNECTED?!?");
+
+      gtalk.login(user, pass, "natty");
+
+      final Roster roster = gtalk.getRoster();
+      roster.addRosterListener(new RosterListener() {
+
+        @Override
+        public void presenceChanged(final Presence pres) {
+          final String from = pres.getFrom();
+          //System.out.println("GOT PRESENCE: "+from);
+          if (from.contains("natty")) {
+            final Message msg = new Message();
+            msg.setTo(from);
+            //System.out.println("Sending message: " + message);
+            msg.setBody(message);
+              
+            gtalk.sendPacket(msg);
+          }
+        }
+
+      @Override
+        public void entriesUpdated(Collection<String> arg0) {
+        }
+
+      @Override
+        public void entriesDeleted(Collection<String> arg0) {
+        }
+
+      @Override
+        public void entriesAdded(Collection<String> arg0) {
+        }
+      });
+
+      final Collection<RosterEntry> entries = roster.getEntries();
+      for (final RosterEntry re : entries) {
+        System.out.println(re.getUser());
+      }
+
     }
 
-    private static void runNatty(final String target) throws IOException {
-        final List<String> command = new ArrayList<String>();
-        command.add("./natty");
-        command.add("-offer");
+    public static void main(final String[] args) throws Exception {
+        // Create a connection to the jabber.org server on a specific port.
+        Natty natty = new Natty();
+        final String user = args[0];
+        final String pass = args[1];
 
+        natty.connectGTalk(user, pass);
+        natty.run("-offer");
+    }
 
-        final ProcessBuilder builder = new ProcessBuilder(command);
-        Map<String, String> environ = builder.environment();
-
-        final Process process = builder.start();
+    private void readResponse() throws IOException, InterruptedException {
+      BufferedReader br = null;
+      try { 
         final InputStream is = process.getInputStream();
         final InputStreamReader isr = new InputStreamReader(is);
-        final BufferedReader br = new BufferedReader(isr);
+        br = new BufferedReader(isr);
         String line;
         while ((line = br.readLine()) != null) {
           System.out.println(line);
+          if (line.contains("offer") || line.contains("answer")) {
+            message = line;
+            break;
+          }
         }
-        System.out.println("Program terminated!");
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+      finally {
+        if (br != null) {
+          br.close();
+        }
+      }
+    }
+
+    private void sendResponse() throws IOException, InterruptedException {
+
+      final OutputStream stdin = process.getOutputStream (); 
+      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+      System.out.println("Writing message " + message);
+      writer.write(message);
+      writer.newLine();
+      writer.close();
+      readResponse();
+    }
+
+    private void run(final String target) throws IOException, InterruptedException, IOException {
+        final List<String> command = new ArrayList<String>();
+        command.add("./natty");
+        command.add(target);
+
+        builder = new ProcessBuilder(command);
+        builder.redirectErrorStream(true);
+
+        Map<String, String> environ = builder.environment();
+
+        process = builder.start();
+
+        sendResponse();
     }
 
 }
