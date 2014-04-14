dab313b3396dbc82ab8f7a8dcff38d28aed587be
Myles Horton <myles@getlantern.org>
2014-04-14 09:00:59 -0700

    added auto-smack install

--- run.bash
+++ run.bash
@@ -4,6 +4,13 @@ function die() {
   echo $*
   exit 1
 }
+
+function installsmack() {
+  mvn install:install-file -Dfile=smack-3.4.1-0cec571.jar -DgroupId=org.jivesoftware -DartifactId=smack -Dversion=3.4.1-0cec571 -Dpackaging=jar || die "Could not install smack"
+  mvn install:install-file -Dfile=smackx-3.4.1-0cec571.jar -DgroupId=org.jivesoftware -DartifactId=smackx -Dversion=3.4.1-0cec571 -Dpackaging=jar || die "Could not install smackx"
+}
+
+test -f target/*shade* || installsmack
 mvn install -Dmaven.test.skip=true
 
 jar=`ls target/*shade.jar | sort | tail -n 1`
