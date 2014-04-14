74649cffcb6cf214a57cd2739d2e02655182e32f
Myles Horton <myles@getlantern.org>
2014-04-14 11:55:29 -0700

    Fixed test

--- src/test/java/org/lantern/natty/SendFileTest.java
+++ src/test/java/org/lantern/natty/SendFileTest.java
@@ -1,22 +1,24 @@
 package org.lantern.natty;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class SendFileTest {
 
     public static void main(String[] args) throws Exception {
-        /*
         startNettyServer();
         Thread.sleep(300);
         final File file = new File("pom.xml");
-        final SendFileClient client = new SendFileClient("127.0.0.1", 7777, file);
+        
+        final URI local = new URI("natty://127.0.0.1:7776");
+        final URI remote = new URI("natty://127.0.0.1:7777");
+        final SendFileClient client = new SendFileClient(local, remote, file);
         client.run();
-        */
     }
 
-    /*
-    private static void startNettyServer() {
-        final SendFileServer server = new SendFileServer(7777);
+    private static void startNettyServer() throws URISyntaxException {
+        final SendFileServer server = new SendFileServer(new URI("natty://127.0.0.1:7777"));
         final Runnable runner = new Runnable() {
             
             @Override
@@ -33,6 +35,5 @@ public class SendFileTest {
         t.setDaemon(true);
         t.start();
     }
-    */
 
 }
